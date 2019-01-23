b82a617089750b01aac92b378a874b1c38e87930
Student <example@example.com>
2019-01-23 10:47:43 -0800

    create app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  recipes: Recipe[] = [
+    new Recipe("Cookies", ["Flour", "Sugar", "1 Egg", "Baking Powder"], ["Mix", "Bake", "Enjoy"]),
+    new Recipe("Cakes", ["Flour", "Sugar", "1 Egg", "Oil"], ["Mix", "Bake", "Enjoy"]),
+    new Recipe("Tacos", ["Cilantro", "Beans"], ["Mix", "Bake", "Enjoy"])
+  ];
+  selectedRecipe = null;
+
+  toggleDetails(currentRecipe){
+    currentRecipe.showDetails = !currentRecipe.showDetails;
+  }
+
+  editRecipe(recipe){
+    this.selectedRecipe = recipe;
+    console.log(this.selectedRecipe.title);
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
+  randomColor() {
+    let number1: number = Math.floor(Math.random()*255+1);
+    let number2: number = Math.floor(Math.random()*255+1);
+    let number3: number = Math.floor(Math.random()*255+1);
+
+    let randomColor: string = `rgb(${number1}, ${number2}, ${number3})`;
+    return randomColor;
+  }
+
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,6 @@
+export class Recipe {
+  public showDetails: boolean = false;
+  constructor(public title: string, public ingredients: string[], public directions: string[]) {
+  }
+
+}
